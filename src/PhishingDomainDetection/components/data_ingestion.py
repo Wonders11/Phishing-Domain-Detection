9826023a249399a1cec27fd41a1e0a869d2ff2c4
Mohit_100 <mohithedaoo2000@gmail.com>
2024-07-19 17:54:48 +0530

    Added return statement

--- src/PhishingDomainDetection/components/data_ingestion.py
+++ src/PhishingDomainDetection/components/data_ingestion.py
@@ -41,6 +41,11 @@ class DataIngestion:
 
             logging.info("Data Ingestion Completed")
 
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
         except Exception as e:
             logging.info("Exception occured during Data Ingestion Stage")
             raise customexception(e,sys)
