140e75501a018685680574b74a920643ca0b2aba
Mohit_100 <mohithedaoo2000@gmail.com>
2024-07-22 20:10:12 +0530

    updated data transformation

--- src/PhishingDomainDetection/components/data_transformation.py
+++ src/PhishingDomainDetection/components/data_transformation.py
@@ -68,14 +68,28 @@ class DataTransformation:
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
             target_feature_train_df = train_df[target_column_name]
 
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
             # transforming train data
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.transform(target_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
             logging.info("Applied preprocessing object on training and testing datasets")
+
+            # we will get o/p in the form of numpy array
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_file_path
                 obj = preprocessing_obj
+            )
+
+            logging.info("Preprocessor pickle file saved")
+
+            return (
+                train_arr,
+                test_arr
             )
 
         except Exception as e:
